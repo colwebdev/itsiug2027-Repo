f4a0c96cf75c6e185e3a481755f348b8b1e95a9a
Bobby Bezuidenhout <colweb@me.com>
2026-08-25 17:07:28 +0200

    Update after changes on Live Server

---
+++ sites/default/files/php/trash/Drupal__node__NodeStorageTrash6a87e025412ec/FnWv5PNv3qpQI6G3uOSfGF5AboWUfDW3gPeR2O5IqjM.php
@@ -0,0 +1,12 @@
+<?php
+
+use Drupal\trash\TrashStorageTrait;
+
+/**
+ * Provides a custom storage class for trash-enabled entity types.
+ */
+class Drupal__node__NodeStorageTrash6a87e025412ec extends \Drupal\node\NodeStorage {
+
+  use TrashStorageTrait;
+
+}
